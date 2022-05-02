d37bc191179fe64c580a45c05285655ed98c6bc7
dommilosz <dommilosz@gmail.com>
2022-05-02 12:17:45 +0200

    Initial commit

---
+++ PicoEasyReflash.h
@@ -0,0 +1,34 @@
+#include "PicoBootsel.h"
+#include "hardware/watchdog.h"
+
+int CheckRestart()
+{
+	if (get_bootsel_button())
+	{
+		int start = millis();
+		while (get_bootsel_button())
+		{
+			if ((millis() - start) > 2000)
+			{
+				return 0;
+			}
+			delay(50);
+			watchdog_update();
+		}
+		delay(50);
+		return 1;
+	}
+	return -1;
+}
+
+void PerformRestart()
+{
+	int r = CheckRestart();
+	if (r != -1)
+	{
+		watchdog_enable(1, 1);
+		while (1)
+		{
+		};
+	}
+}
