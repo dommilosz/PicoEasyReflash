ede2322a861a7064af9cab4d07a659a30fb99fa5
dommilosz <dommilosz@gmail.com>
2022-05-02 17:49:25 +0200

    Update example: add EasyReflashSetDelays

--- examples/Blink/Blink.ino
+++ examples/Blink/Blink.ino
@@ -10,9 +10,13 @@ void setup() {
   delay(100);
   digitalWrite(LED_BUILTIN, LOW);
   delay(100);
+
+  //OPTIONAL: Set timings (50ms - reset, 5s - bootloader)
+  EasyReflashSetDelays(50, 5000);
 }
 
 void loop() {
+  //Tick to check for bootsel press and restart if needed.
   TickEasyReflash();
 
   //code to blink LED asynchronously.
