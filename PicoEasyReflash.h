059f4b197597ab7f05d1ffa313b5ff45b4aeed81
dommilosz <dommilosz@gmail.com>
2022-05-02 16:51:18 +0200

    Add core pausing. Make the functions not block current execution

--- PicoEasyReflash.h
+++ PicoEasyReflash.h
@@ -1,34 +1,49 @@
 #include "PicoBootsel.h"
 #include "hardware/watchdog.h"
 
-int CheckIfRestartNeeded()
-{
-	if (get_bootsel_button())
-	{
-		int start = millis();
-		while (get_bootsel_button())
-		{
-			if ((millis() - start) > 1000)
-			{
+bool EasyReflashPauseCore = true;
+int _bootsel_press_start = -1;
+
+void EasyReflashReset(){
+	watchdog_enable(1, 1);
+	while (1){
+		delay(100);
+		//wait for watchdog reset.
+	}
+}
+
+bool EasyReflashGetButtonState(){
+	if(EasyReflashPauseCore)rp2040.idleOtherCore();
+	bool btn = get_bootsel_button();
+	if(EasyReflashPauseCore)rp2040.resumeOtherCore();
+	return btn;
+}
+
+int TickBootselRead(){
+	
+	//_bootsel_press_duration
+	if (EasyReflashGetButtonState()){
+		if(_bootsel_press_start < 0)_bootsel_press_start = millis();
+		int duration = millis()-_bootsel_press_start;
+		if(duration>1000){
+			return 1;
+		}
+	}else{
+		if(_bootsel_press_start > 0){
+			int duration = millis()-_bootsel_press_start;
+			if(duration>50){
 				return 0;
 			}
-			delay(50);
-			watchdog_update();
 		}
-		delay(50);
-		return 1;
 	}
 	return -1;
 }
 
 void TickEasyReflash()
 {
-	int r = CheckIfRestartNeeded();
+	int r = TickBootselRead();
 	if (r != -1)
 	{
-		watchdog_enable(1, 1);
-		while (1)
-		{
-		};
+		EasyReflashReset();
 	}
 }
